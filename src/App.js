abcb07aec2b744ed529e699c33c8878987db797c
Ajith Revildo <91174414+AjithRevildo@users.noreply.github.com>
2022-03-02 15:39:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+import { Flowers } from './Flowers';
+import { Add } from './Add';
+import { Link, Route, Switch } from "react-router-dom";
+import { Editbouquet } from './Editbouquet';
+import { Bouquetdetail } from './Bouquetdetail';
+
+
+export function App() {
+  return (
+    <div className="App">
+      <fieldset className="revil">
+     <nav>
+        <ul>
+          <li><Link to="/bouquet">Flowers</Link></li>
+          <li><Link to="/newbouquet">Add New Flowers</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/bouquet">
+          <Flowers />
+        </Route>
+        <Route path="/newbouquet">
+          <Add />
+        </Route>
+        <Route path="/bouquet/edit/:id">
+          <Editbouquet />
+        </Route>
+        <Route path="/bouquet/detail/:id">
+          <Bouquetdetail />
+        </Route>
+        <Route exact path="/">
+          <Flowers />
+        </Route>
+        <Route path="/**">
+          <img className="notfound" src="https://i.pinimg.com/originals/11/dc/96/11dc96d2e4daca3ea8ff6aa41299b5e1.gif"
+            alt="404 not found"></img>
+        </Route>
+      </Switch>
+</fieldset>
+
+    </div>
+  );
+}
+
+
